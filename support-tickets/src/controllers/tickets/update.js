7544aabcbecd2e620487134fe828f6db476ec221
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2026-05-16 16:48:54 -0300

    adiciona funcionalidade de atualização de tickets e implementa método de atualização no banco de dados

--- support-tickets/src/controllers/tickets/update.js
+++ support-tickets/src/controllers/tickets/update.js
@@ -1,5 +1,12 @@
 export function update ({ request, response, database }) {
     const { id } = request.params
     const { equipment, description } = request.body
-    return response.end(description)
+
+    
+    database.update ("tickets", id, {
+        equipment,
+        description,
+        update_at: new Date()
+    })
+    return response.end()
 }
--- support-tickets/src/database/database.js
+++ support-tickets/src/database/database.js
@@ -28,7 +28,7 @@ export class Database {
             this.#persist()
     }
 
-    select(table, filters) {
+    select (table, filters) {
         let data = this.#database[table] ?? []
 
         if (filters) {
@@ -39,5 +39,17 @@ export class Database {
             })   
         }
         return data
-    }    
+    }
+
+    update (table, id, data) {
+        const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+
+        if (rowIndex > -1) {
+            this.#database[table][rowIndex] = {
+                ...this.#database[table][rowIndex],
+                ...data 
+            }
+            this.#persist()
+        }
+    }
 }
