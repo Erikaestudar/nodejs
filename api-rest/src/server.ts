200dfca94fbcb0ede7003061edfe1364a19db860
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2026-05-30 18:13:46 -0300

    utilizando o método POST com dados no body da requisição e retornando resposta com JSON e statuscode

--- api-rest/src/server.ts
+++ api-rest/src/server.ts
@@ -4,12 +4,22 @@ const PORT = 3333
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/products", (request, response) => {
     //const { id, user } = request.params
 
     const { page, limit } = request.query
 
      response.send(`Página ${ page } de ${ limit }`)
+})
+
+app.post("/products", (request, response) => {
+    const { name, price } = request.body
+
+    //response.send(`Produto ${ name } custa $ ${ price }`)
+
+    response.status(201).json({ name, price})
 })
 
 app.listen(PORT, () => console.log(`server is running at ${PORT}`))
