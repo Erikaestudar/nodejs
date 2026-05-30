e81b2331a52358f64c1355bee4fd87f085f6aca6
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2026-05-30 17:38:54 -0300

    enviando requisições com método GET e recuperando parâmetros (params e query)

--- api-rest/src/server.ts
+++ api-rest/src/server.ts
@@ -4,4 +4,12 @@ const PORT = 3333
 
 const app = express()
 
+app.get("/products", (request, response) => {
+    //const { id, user } = request.params
+
+    const { page, limit } = request.query
+
+     response.send(`Página ${ page } de ${ limit }`)
+})
+
 app.listen(PORT, () => console.log(`server is running at ${PORT}`))
